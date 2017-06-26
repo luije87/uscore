4c22822b1c7bc3ff3acf4d41a040dea8ec66d1c6
luije87 <luije87@gmail.com>
2017-06-25 23:10:47 -0400

    1
    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 
 @NgModule({
@@ -11,7 +11,8 @@ import { MyApp } from './app.component';
     MyApp,
   ],
   imports: [
-    BrowserModule,       
+    BrowserModule,  
+    HttpModule,     
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @IonicPage({
-  name : 'News'
+  name: 'News'
 })
 @Component({
   selector: 'page-news',
@@ -10,12 +12,15 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
 })
 export class News {
   //public item : any;
-  public item = 
-      { timeago : '11d', date: 'November 5, 1955', src : 'Fox Sports', srcimg: 'assets/img/marty-avatar.png', description : 'Wait a minute. Wait a minute, Doc. Uhhh...', img : 'assets/img/advance-card-bttf.png'};
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController) {
-      // this.item = this.navParams.get('item');
-      // console.log(this.item);
+  public item =
+  { timeago: '11d', date: 'November 5, 1955', src: 'Fox Sports', srcimg: 'assets/img/marty-avatar.png', description: 'Wait a minute. Wait a minute, Doc. Uhhh...', img: 'assets/img/advance-card-bttf.png' };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private viewCtrl: ViewController,public http: Http) {
+    // this.item = this.navParams.get('item');
+    // console.log(this.item);
+    this.http.get('http://localhost:58349/api/default').map(res => res.json()).subscribe(data => {
+        console.log(data);
+    });
   }
 
   ionViewDidLoad() {
@@ -25,5 +30,5 @@ export class News {
   ionViewWillEnter() {
     this.viewCtrl.showBackButton(true);
   }
-  
+
 }
