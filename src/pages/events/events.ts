e69b3435ed9ba67f096f027b6237635dd737b3ac
Luis E Acosta <luije87@gmail.com>
2017-08-09 13:45:07 -0400

    events all, today, upcoming

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, LoadingController } from 'ionic-angular';
 import { EventService } from '../../providers/event-service';
+import { DatePipe } from '@angular/common';
 
 /**
  * Generated class for the Events page.
@@ -15,21 +16,121 @@ import { EventService } from '../../providers/event-service';
   providers : [EventService]
 })
 export class Events {
-    public people: any;
+    public events: any;
+    public loading:any;
+    public data: any;
+    appType = 'All';
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public eventService: EventService) {
-    this.loadEvents();
+  constructor(public navCtrl: NavController, public navParams: NavParams, public eventService: EventService, public loadingController: LoadingController) {
+    this.events = [];
+    this.presentLoadingDefault(); 
+    
   }
 
+ presentLoadingDefault() {
+    this.loading = this.loadingController.create({
+    content: 'Please wait...'
+  });
+
+  this.loading.present();
+  this.loadEvents(this.appType);
+}
+
+
+  
   ionViewDidLoad() {
     console.log('ionViewDidLoad Events');
   }
 
-  loadEvents(){
-    this.eventService.load()
-  .then(data => {
-    this.people = data;
-    console.log(this.people);
-  });
+  /*doInfinite(infiniteScroll) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      for (let i = 0; i < 30; i++) {
+        this.people.push( this.people.length );
+      }
+
+      console.log('Async operation has ended');
+      infiniteScroll.complete();
+    }, 500);
+  }*/
+
+  apps: any = {
+    'All': [
+      {
+        name: 'Monopoly',
+        price: '$0.99'
+      },
+      {
+        name: 'Angry Birds',
+        price: '$2.99'
+      }
+    ],
+    'Today': [
+      {
+        name: 'Snapchat',
+        price: 'GET'
+      },
+      {
+        name: 'Instagram',
+        price: 'OPEN'
+      }
+    ],
+    'Upcoming': [
+      {
+        name: 'Spotify',
+        price: 'OPEN'
+      },
+      {
+        name: 'Pandora',
+        price: 'GET'
+      }
+    ]
+  };
+
+   getItems(type: any) {
+    if(this.data !== undefined){
+      this.data.forEach(element => {
+        if(new Date(element.StartDate) >= new Date()){
+          this.events.push(element);
+        }
+      });
+    }
+
+    return this.events;
+  }
+
+  loadEvents(type: any){
+    this.events = [];
+    this.eventService.load().then(data => 
+    {   
+      switch(type){
+        case 'All':
+        data.forEach(element => {
+          this.events.push(element);
+        });
+        break;
+        case 'Today':
+        data.forEach(element => {          
+           if(new Date(element.StartDate) === new Date()){
+             console.log('Today case');
+            this.events.push(element);
+          }
+        });
+        break;
+        case 'Upcoming':
+        var date = new Date();
+        date.setDate(date.getDate() + 7);
+        data.forEach(element => {          
+          
+           if(new Date(element.StartDate) <= date){
+             console.log('Today case');
+            this.events.push(element);
+          }
+        });
+        break;
+      }
+      this.loading.dismiss();
+    });
   }
 }
--- src/providers/event-service.ts
+++ src/providers/event-service.ts
@@ -27,14 +27,15 @@ export class EventService {
     // We're using Angular HTTP provider to request the data,
     // then on the response, it'll map the JSON data to a parsed JS object.
     // Next, we process the data and resolve the promise with the new data.
-    this.http.get('https://randomuser.me/api/?results=10')
+    this.http.get('https://webservices.stu.edu/api/events/0')
       .map(res => res.json())
       .subscribe(data => {
-        
+
         // we've got back the raw data, now generate the core schedule data
         // and save the data for later reference
-        this.data = data.results;
-        resolve(this.data);
+        this.data = data;
+        resolve(this.data); 
+        
       });
   });
 }
